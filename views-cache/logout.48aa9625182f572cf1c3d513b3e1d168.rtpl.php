54984668570f5cccb6c78ffc471057f68adc0cd1
BlackBashi <arlindomauricio2013sk8@gmail.com>
2019-11-07 14:02:32 -0400

    gitignore

--- views-cache/logout.48aa9625182f572cf1c3d513b3e1d168.rtpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="container">
-<div class="p-3 mb-2 bg-light text-dark" id="tmcont" style="font: normal 23px Robo" >
-    <h1 class="text-center">Você saiu!</h1><br>
-    <form action="/">
-        <input class="btn btn-success" type="submit" value="voltar">
-    </form>
-</div>
-</div>
